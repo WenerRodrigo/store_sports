0f50c5858aa2b28c3ab14a0495119212a664c3ec
Wener Rodrigo <wener19.wr@gmail.com>
2023-08-28 20:17:06 -0300

    inicio do projeto

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>Mega Store do Esporte</h1>
+            <div>
+                <ul>
+                    <li>Produtos</li>
+                    <li>Contato</li>
+                    <li>Sobre</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
