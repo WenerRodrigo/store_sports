9574a2275c1641ec9c1661214c387af42d9f75e4
Wener Rodrigo <wener19.wr@gmail.com>
2023-08-29 22:25:54 -0300

    Criado um formulario pra contato, criado tambérm o seu css e algumas validações de envio

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,87 @@
+import { useState } from "react";
+import styles from './Contact.module.css';
+import Header from '../../components/header/Header';
+
+
+
 const Contact = () => {
+    const [formData, setFormData] = useState({
+        nome: '',
+        email: '',
+        telefone: '',
+        mensagem: ''
+    })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Dados do formulario enviados com sucesso:", formData);
+        setFormData({
+            nome: '',
+            email: '',
+            telefone: '',
+            mensagem: ''
+        })
+    }
+
+
     return (
-        <div>
-            <h2>Contato</h2>
+        <>
+        <Header />
+            <div className={styles.container}>
+            <h2 className={styles.title}>Deixe sua mensagem</h2>
+            <form className={styles.form} onSubmit={handleSubmit}>
+                <div className={styles.campos}>
+                    <label className={styles.label} htmlFor="nome">Nome:</label>
+                    <input
+                        type="text"
+                        id="nome"
+                        name="nome"
+                        value={formData.nome}
+                        onChange={handleChange}
+                        placeholder="Digite seu nome"
+                    />
+                </div>
+                <div className={styles.campos}>
+                    <label className={styles.label} htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        placeholder="Digite seu Email"
+                    />
+                </div>
+                <div className={styles.campos}>
+                    <label className={styles.label} htmlFor="nome">Telefone:</label>
+                    <input
+                        type="tel"
+                        id="telefone"
+                        name="telefone"
+                        value={formData.telefone}
+                        onChange={handleChange}
+                        placeholder="Digite seu Telefone"
+                    />
+                </div>
+                <div className={styles.campos}>
+                    <label className={styles.label} htmlFor="nome">Mensagem:</label>
+                    <textarea
+                        id="mensagem"
+                        name="mensagem"
+                        value={formData.mensagem}
+                        onChange={handleChange}
+                        placeholder="Escreva sua mensagem..."
+                    />
+                </div>
+                <button className={styles.botao} type="submit" >Enviar</button>
+            </form>
         </div>
+        </>
     )
 }
 
